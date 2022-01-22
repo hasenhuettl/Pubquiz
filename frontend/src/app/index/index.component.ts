5eafe98e2ed82f76b5e93d619b9c46ecbf98220c
hasenhuettl <hasi1999@gmx.at>
2022-01-22 23:20:06 +0100

    sorting in index

--- frontend/src/app/index/index.component.ts
+++ frontend/src/app/index/index.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 import {Quiz,QuizService} from "../services/quiz.service";
 import {FormControl} from "@angular/forms";
 import {ActivatedRoute} from "@angular/router";
@@ -10,42 +9,47 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-      displayedColumns = ['id', 'quiz_name','join','edit','delete'];
-      quizzes: Quiz[] = [];
-      filteredActivities: Quiz[] = [];
-      filterFormControl = new FormControl('');
+  displayedColumns = ['id', 'quiz_name', 'join', 'edit', 'delete'];
+  quizzes: Quiz[] = [];
+  filteredQuizzes: Quiz[] = [];
+  filterFormControl = new FormControl('');
 
 
   constructor(private quizService: QuizService,
               private route: ActivatedRoute,
-              private http: HttpClient,
-  )  {  }
+  ) {  }
 
   ngOnInit(): void {
-    /**this.quizService.getQuizzes().subscribe(quizzes => this.quizzes = quizzes)**/
+    //this.quizService.getQuizzes().subscribe(quizzes => this.quizzes = quizzes)
     this.quizService.getQuizzes().subscribe((response) => {
       //console.log({response})
       this.quizzes = response
+      this.filteredQuizzes = this.quizzes
     })
 
     this.filterFormControl.valueChanges.subscribe(value => this.filter(value));
-    this.route.paramMap.subscribe(params => { this.filterFormControl.setValue(params.get('filter')) });
+    this.route.paramMap.subscribe(params => {
+      this.filterFormControl.setValue(params.get('filter'))
+    });
   }
 
+
   filter(filterValue: string) {
-    this.filteredActivities = this.quizzes.filter(a => {
+    this.filteredQuizzes = this.quizzes.filter(a => {
         return !filterValue || a.quiz_name.toLowerCase().includes(filterValue.toLowerCase())
       }
     )
   }
 
   deleteQuiz(quiz: Quiz): void {
-    this.quizService.deleteQuiz(quiz).subscribe( () => { this.ngOnInit() });
+    this.quizService.deleteQuiz(quiz).subscribe(() => {
+      this.ngOnInit()
+    });
   }
-  // confirmation dialog for deleting sport entries
+
   confirmDelete(quiz: Quiz) {
     if (confirm("Are you sure to delete '" + quiz.quiz_name + "'?")) {
-      console.log(this.deleteQuiz(quiz))
+      this.deleteQuiz(quiz)
     }
   }
 }
